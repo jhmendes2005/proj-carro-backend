7084d32714ea3fe784d5604b2d5c15dcd1697c95
jhmendes2005 <jhmendes2005@gmail.com>
2025-12-01 21:50:44 -0300

    first3

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 👇 Adicione esta linha
-  //app.enableCors({
-  //  origin: 'https://meu-site-de-carros.com', // Só permite seu site real
-  //});
   app.enableCors();
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT || 3001, '0.0.0.0');
 }
+
 bootstrap();
